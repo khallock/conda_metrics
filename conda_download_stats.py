6534f798fbf1c10e0e3d3c7a9a0f43d3af52a581
Kevin Hallock <hallock@ucar.edu>
2017-03-21 13:30:25 -0600

    Implemented basic sqlite3 backend
    We still need to be able to generate reports from the database.

--- conda_download_stats.py
+++ conda_download_stats.py
@@ -3,7 +3,12 @@ import argparse
 from binstar_client.utils import get_server_api, spec
 import binstar_client.errors
 
+import sqlite3
 import sys
+
+# set up sqlite
+conn = sqlite3.connect('conda_metrics.db')
+c = conn.cursor()
 
 args = argparse.Namespace()
 args.token = None
@@ -20,19 +25,24 @@ if len(sys.argv) > 1:
 for package in packages:
     package_total_dls = 0
     for conda_channel in conda_channels:
-    #    s = spec.PackageSpec(conda_channel, package=package)
+        #db_table = "%s_%s" % (package, conda_channel)
+        db_table = package.replace('-', '')
 
         aserver_api = get_server_api(args.token, args.site, args.log_level)
         try: package_obj = aserver_api.package(conda_channel, package)
         except binstar_client.errors.NotFound: continue
-        channel_total_dls = 0
+        try:
+            c.execute('''CREATE TABLE %s (channel text, version text, build text, platform text, arch text, pyversion text, npversion text, uploaded_date date, downloads int, check_date datetime default CURRENT_TIMESTAMP)''' % db_table)
+            conn.commit()
+        except sqlite3.OperationalError:
+            pass
         for version_str in package_obj['versions']:
             version = aserver_api.release(conda_channel, package, version_str)
             for d in version['distributions']:
                 #distribution_os = d['attrs']['operatingsystem']
-                distribution_os = d['attrs']['machine']
+                distribution_os = d['attrs']['platform']
                 #distribution_arch = d['attrs']['arch']
-                distribution_arch = d['attrs']['platform']
+                distribution_arch = d['attrs']['machine']
                 distribution_build = d['attrs']['build']
                 distribution_downloads = d['ndownloads']
                 distribution_upload_time = d['upload_time']
@@ -50,9 +60,7 @@ for package in packages:
                 #for key in d.keys():
                 #    print "%s: %s" % (key, d[key])
                 #sys.exit(0)
-                print conda_channel, package, version_str, distribution_build, distribution_os, distribution_arch, distribution_downloads, py_version, np_version, distribution_upload_time
-                channel_total_dls += distribution_downloads
-        package_total_dls += channel_total_dls
-        print "total downloads of", conda_channel, package, channel_total_dls
-    print "total downloads of", package, package_total_dls
-    print ""
+                c.execute('''INSERT INTO %s (channel, version, build, platform, arch, pyversion, npversion, uploaded_date, downloads) VALUES (?,?,?,?,?,?,?,?,?)''' % db_table,
+                             (conda_channel, version_str, distribution_build, distribution_os, distribution_arch, py_version, np_version, distribution_upload_time, distribution_downloads))
+                conn.commit()
+conn.close()
